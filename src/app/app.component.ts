4eb270d0bd79a99cbcd78354c5d6cf97bd4c0dbc
Tomasz Rogala <tomasz.rogala1505@gmail.com>
2021-11-07 16:55:26 +0100

    cards app done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title:'Neet Tree',
+      imageURL: 'assets/tree.jpeg',
+      userName: 'nature',
+      content: 'I sow this tree today'
+    },
+    {
+      title:'Snowy Mountain',
+      imageURL: 'assets/mountain.jpeg',
+      userName: 'mountainlover',
+      content: 'Here is a picture of snowy mountain!'
+    },
+    {
+      title:'Mountain Biking',
+      imageURL: 'assets/biking.jpeg',
+      userName: 'biking122223',
+      content: 'I did some biking tofay'
+    },
+  ]
 }
